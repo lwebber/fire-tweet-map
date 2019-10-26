2caf160d2e949d42475e80fea7b247b4e227bf2c
Laura Webber <wwebby1@gmail.com>
2019-10-26 10:49:04 -0700

    build map

---
+++ index.js
@@ -0,0 +1,8 @@
+var mymap = L.map('mapid').setView([37.9735346, -122.5310874], 11);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1Ijoid3dlYmJ5MSIsImEiOiJjazI3dWxjd2QxNWhsM2R0Y2gzaDZhYTZ0In0.ank4076W4hJPslV-NAUkNg'
+}).addTo(mymap);
