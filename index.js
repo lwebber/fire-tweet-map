989cbe55b0f991db5fd82afa99f05cbab2803517
Laura Webber <wwebby1@gmail.com>
2019-10-26 11:00:50 -0700

    add popup

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1Ijoid3dlYmJ5MSIsImEiOiJjazI3dWxjd2QxNWhsM2R0Y2gzaDZhYTZ0In0.ank4076W4hJPslV-NAUkNg'
 }).addTo(mymap);
+
+var popup = L.popup()
+    .setLatLng([37.9735346, -122.5310874])
+    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    .openOn(mymap);
